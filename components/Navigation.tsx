406eb55ad1455def1d7117f06acad75277753251
Hrei2 <haralds.re@gmail.com>
2025-12-06 15:36:39 +0200

    removed blog page

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -21,7 +21,6 @@ export default function Navigation() {
         { href: '/', label: 'Home' },
         { href: '/about', label: 'About' },
         { href: '/projects', label: 'Projects' },
-        { href: '/blog', label: 'Blog' },
         { href: '/contact', label: 'Contact' },
     ];
 
--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
     reactStrictMode: true,
     images: {
         remotePatterns: [],
